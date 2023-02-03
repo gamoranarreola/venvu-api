669dfa987a77440d8ae81bdc6be60c289cb9456a
gamoranarreola <corazonymente@icloud.com>
2023-02-03 03:06:31 -0800

    minor fixes

--- bin/start_celery_worker.sh
+++ bin/start_celery_worker.sh
@@ -1,2 +1,2 @@
 cd src
-pipenv run celery -A app.celery worker --pool=solo --loglevel=info
+python3 -m pipenv run celery -A app.celery worker --pool=solo --loglevel=info
--- src/app/api/routes.py
+++ src/app/api/routes.py
@@ -1,45 +1,96 @@
 from flask_restful import Api
 
 from app.api.accounts import AccountApi, AccountListApi
-from app.api.company_profiles import (CompanyProfileApi, CompanyProfileListApi,
-                                      CompanyTypeApi, CompanyTypeListApi,
-                                      CountriesListApi, EmployeeCountRangeApi,
-                                      EmployeeCountRangeListApi, IndustryApi,
-                                      IndustryListApi, ProvincesListApi,
-                                      YearlyRevenueRangeApi,
-                                      YearlyRevenueRangeListApi)
+from app.api.company_profiles import (
+    CompanyProfileApi,
+    CompanyProfileListApi,
+    CompanyTypeApi,
+    CompanyTypeListApi,
+    CountriesListApi,
+    EmployeeCountRangeApi,
+    EmployeeCountRangeListApi,
+    IndustryApi,
+    IndustryListApi,
+    ProvincesListApi,
+    YearlyRevenueRangeApi,
+    YearlyRevenueRangeListApi
+)
 from app.api.roles import RolesListApi
 
 
 def create_routes(api: Api):
+
     api.add_resource(
-        CompanyProfileApi, "/api/company-profiles/<int:company_profile_id>"
-    )  # noqa: E501
-    api.add_resource(CompanyProfileListApi, "/api/company-profiles")
-    api.add_resource(AccountApi, "/api/accounts/<int:account_id>")
-    api.add_resource(AccountListApi, "/api/accounts")
-    api.add_resource(CompanyTypeListApi, "/api/company-profiles/company-types")
+        CompanyProfileApi,
+        "/api/company-profiles/<int:company_profile_id>"
+    )
+
     api.add_resource(
-        CompanyTypeApi, "/api/company-profiles/company-types/<string:company_type_name>"
-    )  # noqa: E501
+        CompanyProfileListApi,
+        "/api/company-profiles"
+    )
+
     api.add_resource(
-        EmployeeCountRangeListApi, "/api/company-profiles/employee-count-ranges"
-    )  # noqa: E501
+        AccountApi,
+        "/api/accounts/<int:account_id>"
+    )
+
+    api.add_resource(
+        AccountListApi,
+        "/api/accounts"
+    )
+
+    api.add_resource(
+        CompanyTypeListApi,
+        "/api/company-profiles/company-types"
+    )
+
+    api.add_resource(
+        CompanyTypeApi,
+        "/api/company-profiles/company-types/<string:company_type_name>"
+    )
+
+    api.add_resource(
+        EmployeeCountRangeListApi,
+        "/api/company-profiles/employee-count-ranges"
+    )
+
     api.add_resource(
         EmployeeCountRangeApi,
-        "/api/company-profiles/employee-count-ranges/<string:employee_count_range_name>",
-    )  # noqa: E501
+        "/api/company-profiles/employee-count-ranges/<string:employee_count_range_name>",  # noqa: E501
+    )
+
     api.add_resource(
-        YearlyRevenueRangeListApi, "/api/company-profiles/yearly-revenue-ranges"
-    )  # noqa: E501
+        YearlyRevenueRangeListApi,
+        "/api/company-profiles/yearly-revenue-ranges"
+    )
+
     api.add_resource(
         YearlyRevenueRangeApi,
-        "/api/company-profiles/yearly-revenue-ranges/<string:yearly_revenue_range_name>",
-    )  # noqa: E501
-    api.add_resource(CountriesListApi, "/api/company-profiles/countries")
-    api.add_resource(ProvincesListApi, "/api/company-profiles/provinces")
-    api.add_resource(IndustryListApi, "/api/company-profiles/industries")
+        "/api/company-profiles/yearly-revenue-ranges/<string:yearly_revenue_range_name>",  # noqa: E501
+    )
+
     api.add_resource(
-        IndustryApi, "/api/company-profiles/industries/<int:industry_id>"
-    )  # noqa: E501
-    api.add_resource(RolesListApi, "/api/user-management/roles")
+        CountriesListApi,
+        "/api/company-profiles/countries"
+    )
+
+    api.add_resource(
+        ProvincesListApi,
+        "/api/company-profiles/provinces"
+    )
+
+    api.add_resource(
+        IndustryListApi,
+        "/api/company-profiles/industries"
+    )
+
+    api.add_resource(
+        IndustryApi,
+        "/api/company-profiles/industries/<int:industry_id>"
+    )
+
+    api.add_resource(
+        RolesListApi,
+        "/api/user-management/roles"
+    )
